4897f1331a1fedb33d2cb2e458acf93a285d58c3
Ferenc Pintér <pinterfer@gmail.com>
2025-01-20 15:11:15 +0100

    Threadpool to platform independent c++, std::vector to fix thread handle leak, Thread-safe singleton using std::call_once, inline member init (c++17)

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -1,112 +1,117 @@
-#pragma once
-
 #include "threadpool.h"
 #include <thread>
 
-Threadpool* Threadpool::instance;
+Threadpool* Threadpool::instance = nullptr;
 
-/**********************************************************************
-* Constructor (private)
-**********************************************************************/
 Threadpool::Threadpool(int _threads) {
-	n_threads = _threads > 0 ? (std::min)((unsigned int)_threads, std::thread::hardware_concurrency()) : std::thread::hardware_concurrency();
-	threads = new tp_struct[n_threads];
+	n_threads = _threads > 0 ?
+		std::min(static_cast<unsigned int>(_threads), std::thread::hardware_concurrency()) :
+		std::thread::hardware_concurrency();
+
+	threads.resize(n_threads);
 
 	for (int i = 0; i < n_threads; ++i) {
-#ifdef _WIN32
-		threads[i].handle = CreateThread(NULL, 1, (LPTHREAD_START_ROUTINE)Thread, &threads[i], 0, NULL);
-#else
-		pthread_create(&threads[i].handle, NULL, TP_Thread, &threads[i]);
-#endif
 		threads[i].main_mutex = &main_mutex;
 		threads[i].return_mutex = &return_mutex;
 		threads[i].main_cond = &main_cond;
 		threads[i].return_cond = &return_cond;
-		threads[i].free = true;
-		threads[i].stop = false;
 		threads[i].queue = &queue;
 		threads[i].i = i;
+
+		threads[i].handle = std::thread(&Threadpool::ThreadFunction, this, &threads[i]);
 	}
 }
 
-/**********************************************************************
-* Destructor
-**********************************************************************/
 Threadpool::~Threadpool() {
-	int i;
-
 	{
 		std::lock_guard<std::mutex> mlock(main_mutex);
-		for (i = 0; i < n_threads; ++i) {
+		for (int i = 0; i < n_threads; ++i) {
 			threads[i].stop = true;
 		}
 	}
 
 	main_cond.notify_all();
-	for (i = 0; i < n_threads; ++i) {
-#ifdef _WIN32
-		WaitForSingleObject(threads[i].handle, INFINITE);
-#else
-		pthread_join(threads[i].handle, NULL);
-#endif
+
+	for (auto& thread : threads) {
+		if (thread.handle.joinable()) {
+			thread.handle.join();
+		}
 	}
 }
 
-/**********************************************************************
-* Threads
-**********************************************************************/
-#define P ((Threadpool::tp_struct*)param)
-
-#ifdef _WIN32
-DWORD WINAPI Threadpool::Thread(void* param) {
-#else
-void* TP_Thread(void* param) {
-#endif
+void Threadpool::ThreadFunction(tp_struct* param) {
 	while (true) {
+		std::function<void()> task;
 		{
-			std::unique_lock<std::mutex> mlock(*P->main_mutex);
-			P->main_cond->wait(mlock, [=]{ return P->queue->size() || P->stop; });
-			if (P->queue->size()) {
-				P->function = P->queue->front();
-				P->queue->pop_front();
-				P->free = false;
+			std::function<void()> task;
+			bool should_stop = false;
+
+			// First check without locking
+			if (param->stop && param->queue->empty()) {
+				break;
+			}
+
+			// Quick check if there's work without locking
+			if (param->queue->empty()) {
+				// Only lock and wait if necessary
+				std::unique_lock<std::mutex> mlock(*param->main_mutex);
+				param->main_cond->wait(mlock, [param] {
+					return param->stop || !param->queue->empty();
+					});
+
+				should_stop = param->stop && param->queue->empty();
+
+				if (!should_stop && !param->queue->empty()) {
+					param->free = false;
+					task = std::move(param->queue->front());
+					param->queue->pop_front();
+				}
+				mlock.unlock();  // Explicitly unlock before potential break
+			}
+			else {
+				// Fast path - queue has work
+				std::unique_lock<std::mutex> mlock(*param->main_mutex);
+				if (!param->queue->empty()) {  // Check again under lock
+					param->free = false;
+					task = std::move(param->queue->front());
+					param->queue->pop_front();
+				}
+			}
+
+			if (should_stop) {
+				break;
+			}
+
+			if (task) {
+				task();  // Execute without holding any locks
+
+				// Minimal locking for status update
+				{
+					std::lock_guard<std::mutex> rlock(*param->return_mutex);
+					param->free = true;
+				}
+				param->return_cond->notify_all();
 			}
 		}
-		if (P->stop) break;
-
-		P->function();
-
-		{
-			std::lock_guard<std::mutex> mlock(*P->return_mutex); // necessary
-			P->free = true;
-		}
-		P->return_cond->notify_all();
-	}
-
-	return 0;
 }
 
-/**********************************************************************
-* Wait
-**********************************************************************/
+}
+
+void Threadpool::Queue(std::function<void()> function) {
+	{
+		std::lock_guard<std::mutex> mlock(main_mutex);
+		queue.push_back(std::move(function));
+	}
+	// Only wake one thread - reduces contention
+	main_cond.notify_one();
+}
+
 void Threadpool::Wait() {
 	std::unique_lock<std::mutex> rlock(return_mutex);
 	return_cond.wait(rlock, [this] {
-		std::lock_guard<std::mutex> mlock(main_mutex);  // Added lock for queue access
-		if (!queue.empty()) return false;
-		for (int i = 0; i < n_threads; ++i) {
-			if (!threads[i].free) return false;
-
-		}
-		return true;
+		std::lock_guard<std::mutex> mlock(main_mutex);
+		return queue.empty() &&
+			std::all_of(threads.begin(), threads.end(),
+				[](const tp_struct& t) { return t.free; });
 		});
-}
-
-/**********************************************************************
-* Queue
-**********************************************************************/
-void Threadpool::Queue(std::function<void()> function) {
-	std::lock_guard<std::mutex> mlock(main_mutex); // not sure what this is for
-	queue.push_back(std::move(function));
-	main_cond.notify_one(); // changed from notify_all()
 }
--- src/threadpool.h
+++ src/threadpool.h
@@ -1,52 +1,60 @@
 #pragma once
 
+#include <thread>
 #include <mutex>
 #include <condition_variable>
-#include <functional>
 #include <deque>
-#ifdef _WIN32
-#include <Windows.h>
-#endif
-
-#ifndef _WIN32
-void* TP_Thread(void* param);
-#endif
+#include <functional>
+#include <algorithm>
+#include <memory>
+#include <vector>
 
 class Threadpool {
 public:
-	static Threadpool* GetInstance(int threads = 0) { if (!instance) instance = new Threadpool(threads); return instance; }
-	void Queue(std::function<void()> function);
-	int GetNThreads() { return n_threads; };
-	void Wait();
-	struct tp_struct {
-#ifdef _WIN32
-		HANDLE handle;
-#else
-		pthread_t handle;
-#endif
-		std::function<void()> function;
-		bool free;
-		bool stop;
-		std::mutex* main_mutex;
-		std::mutex* return_mutex;
-		std::condition_variable* main_cond;
-		std::condition_variable* return_cond;
-		std::deque<std::function<void()>>* queue;
-		int i;
-	};
+  static Threadpool* GetInstance(int threads = 0) {
+    static std::once_flag flag;
+    std::call_once(flag, [&]() {
+      instance = new Threadpool(threads);
+      });
+    return instance;
+  }
+
+  void Queue(std::function<void()> function);
+  int GetNThreads() const { return n_threads; }
+  void Wait();
 
 private:
-	static Threadpool* instance;
-	Threadpool(int _threads = 0); // constructor is private
-	~Threadpool();
-#ifdef _WIN32
-	static DWORD WINAPI Thread(void* param);
-#endif
-	tp_struct* threads;
-	std::deque<std::function<void()>> queue;
-	int n_threads;
-	std::mutex main_mutex;
-	std::mutex return_mutex;
-	std::condition_variable main_cond;
-	std::condition_variable return_cond;
+  struct tp_struct {
+    std::thread handle;
+    std::function<void()> function;
+    bool free{ true };
+    bool stop{ false };
+    std::mutex* main_mutex{ nullptr };
+    std::mutex* return_mutex{ nullptr };
+    std::condition_variable* main_cond{ nullptr };
+    std::condition_variable* return_cond{ nullptr };
+    std::deque<std::function<void()>>* queue{ nullptr };
+    int i{ 0 };
+  };
+
+  static Threadpool* instance;
+  explicit Threadpool(int _threads = 0);  // constructor is private
+  ~Threadpool();
+
+  void ThreadFunction(tp_struct* param);
+
+  std::vector<tp_struct> threads;
+  std::deque<std::function<void()>> queue;
+  int n_threads;
+  std::mutex main_mutex;
+  std::mutex return_mutex;
+  std::condition_variable main_cond;
+  std::condition_variable return_cond;
+
+  // Delete copy constructor and assignment operator
+  Threadpool(const Threadpool&) = delete;
+  Threadpool& operator=(const Threadpool&) = delete;
+  // Delete move constructor and assignment operator
+  Threadpool(Threadpool&&) = delete;
+  Threadpool& operator=(Threadpool&&) = delete;
 };
